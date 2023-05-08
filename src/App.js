b7516b74885be315d27616600ff9aff362237c7c
seyha30 <sinseyha1x@gmail.com>
2023-05-08 14:51:40 +0700

    1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author                : seyha30<sinseyha1x@gmail.com>                     *
  * @CreatedDate           : 2023-05-08 14:45:40                               *
  * @LastEditors           : seyha30<sinseyha1x@gmail.com>                     *
- * @LastEditDate          : 2023-05-08 14:45:40                               *
+ * @LastEditDate          : 2023-05-08 14:51:18                               *
  * @FilePath              : thinking-react/src/App.js                         *
  * @CopyRight             : SAMBAT FINANCE                                    *
  *****************************************************************************/
@@ -11,6 +11,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const products = [
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+  ];
+
   return (
     <div>
       <h1>Main App</h1>
